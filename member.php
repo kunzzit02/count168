565505c18a4d69da1e954e89089168e38a821a50
ZheJin0527 <zjwu0407@gmail.com>
2025-12-19 14:44:18 +0800

    162

--- member.php
+++ member.php
@@ -1136,6 +1136,7 @@ $today = date('d/m/Y');
                     date_from: dateFrom,
                     date_to: dateTo,
                     target_account_id: memberConfig.accountId,
+                    company_id: memberConfig.companyId,
                     show_inactive: '1',
                     hide_zero_balance: '0'
                 });
@@ -1335,7 +1336,8 @@ $today = date('d/m/Y');
                 const params = new URLSearchParams({
                     account_id: memberConfig.accountId,
                     date_from: dateFrom,
-                    date_to: dateTo
+                    date_to: dateTo,
+                    company_id: memberConfig.companyId
                 });
                 if (code) {
                     params.append('currency', code);
