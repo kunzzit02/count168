a74cc7ad3ba4fd752210935aca6e02d68d856de5
ZheJin0527 <zjwu0407@gmail.com>
2025-12-23 09:44:18 +0800

    282

--- sidebar.php
+++ sidebar.php
@@ -65,7 +65,6 @@ $avatarLetter = $name ? strtoupper($name[0]) : 'U';
     .informationmenu,
     .informationmenu * {
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
-        font-weight: normal !important;
     }
 
     /* 用户信息容器（包裹头像和用户信息） */
