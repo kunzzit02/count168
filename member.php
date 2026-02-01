5a3937ed5cefefd8c2c9e550388ff4c39a9bf14c
JK6373 <hwang6373@gmail.com>
2026-02-01 11:35:19 +0800

    04

--- member.php
+++ member.php
@@ -888,8 +888,8 @@ $today = date('d/m/Y');
         }
 
         .member-winloss-table .transaction-history-col-remark {
-            width: 10%;
-            min-width: 90px;
+            width: 20%;
+            min-width: 150px;
         }
 
         .text-uppercase {
