4302ff837ea48bcd6ce19e7fa03cf1f8a1435a9a
JK6373 <hwang6373@gmail.com>
2026-02-10 23:26:18 +0800

    95

--- customer_report.php
+++ customer_report.php
@@ -13,6 +13,9 @@ $isOwner = ($userRole === 'owner');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
     <link href='https://fonts.googleapis.com/css2?family=Amaranth:wght@400;700&display=swap' rel='stylesheet'>
     <title>Customer Report</title>
     <link rel="stylesheet" href="css/accountCSS.css?v=<?php echo time(); ?>" />
